fd6984e873e0561113500a1d8d18b5095acc941a
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-30 15:53:25 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -6,10 +6,34 @@
 using namespace std;
 
 class Solution {
+  bool charLess(char lhs, char rhs, string_view order) {
+    return order.find(lhs) < order.find(rhs);
+  }
+
+  bool wordLessOrEq(string_view lhs, string_view rhs, string_view order) {
+    while (not lhs.empty() and not rhs.empty()) {
+      char l = lhs.front();
+      char r = rhs.front();
+      if (l != r) {
+//        std::cout << l << " < "s << r << " is "s << charLess(l, r, order) << endl;
+        return charLess(l, r, order);
+      }
+      lhs.remove_prefix(1);
+      rhs.remove_prefix(1);
+    }
+
+    return lhs.empty();
+  }
+
  public:
-  bool isAlienSorted(vector<string>& words, string order) {
-    // FIXME
-    return false;
+  bool isAlienSorted(vector<string> &words, string order) {
+    for (int i = 0; i + 1 < words.size(); ++i) {
+      if (not wordLessOrEq(words[i], words[i + 1], order)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 };
 
